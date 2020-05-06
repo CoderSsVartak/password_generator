49f4b73bc68b801e347f15c757bc750daf8d44b1
CoderSsVartak <42352004+CoderSsVartak@users.noreply.github.com>
2020-05-06 18:05:00 +0530

    Updated Imports following the directory structure

---
+++ app.py
@@ -0,0 +1,8 @@
+#import all code files
+from Security.encrypt import encryption
+from send_mail import Send_Mail
+from Security.random_pass import Random_Password
+from Security.security_qns import Security_question
+from Database.db_login_register import Login, Register
+
+print("Import Successfull")
