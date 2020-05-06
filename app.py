353d93d12cd12e54eebc31ffdfe1ee2092fec770
CoderSsVartak <42352004+CoderSsVartak@users.noreply.github.com>
2020-05-06 14:44:27 +0530

    Import all the necessary code

---
+++ app.py
@@ -0,0 +1,6 @@
+#import all code files
+from encrypt import encryption
+from security_qns import Security_question
+from Database.db_login_register import Login, Register
+
+print("Import Successfull")
