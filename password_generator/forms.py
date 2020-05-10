92aba30bed7813719ab0458d08aa22fb9d8f201f
Sahil Vartak <sahilravu213@gmail.com>
2020-05-10 21:17:41 +0530

    Registration class added

--- password_generator/forms.py
+++ password_generator/forms.py
@@ -1,3 +1,4 @@
+import re
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
@@ -16,27 +17,36 @@ class LoginForm(FlaskForm):
         user = User.query.filter_by(username=username.data).first()
         if not user:
             raise ValidationError("Username Already Taken.Choose a different username")
-    
+        
+        elif user == None:
+            raise ValidationError("Username Not Found")
+
+
 
 class RegistrationForm(FlaskForm):
 
-    username = StringField('username', validators=[DataRequired(), Length(min=2, max=20)])
-    email = StringField('email', validators=[DataRequired(), Email()])
-    password = PasswordField('password', validators=[DataRequired()])
-    confirm_password = PasswordField('confirmpassword', validators=[DataRequired(), EqualTo('password')])
-
+    fullname = StringField('fname_ip', validators=[DataRequired(), Length(min=2, max=120)])
+    contact = StringField('phn_ip', validators=[DataRequired(), Length(min=6, max=10)])
+    username = StringField('user_ip', validators=[DataRequired(), Length(min=2, max=120)])
+    email = StringField('email_ip', validators=[DataRequired(), Email()])
+    password = PasswordField('password_ip', validators=[DataRequired()])
+    confirm_password = PasswordField('repassword_ip', validators=[DataRequired(), EqualTo('password')])
+    sec_qns = StringField('security_qns', validators=[DataRequired()])
+    sec_ans = StringField('security_qns_ans', validators=[DataRequired()])
     submit = SubmitField('Sign Up')
+
 
     def validate_username(self, username):
 
         user = User.query.filter_by(username=username.data).first()
         if user:
             raise ValidationError("Username Already Taken.Choose a different username")
+        
 
     def validate_email(self, email):
 
         email = User.query.filter_by(email=email.data).first()
         if email:
-            raise ValidationError("Email Already Taken.Choose a different email")
+            raise ValidationError("Email Already Exists. Please choose a valid Email Address")
 
 
